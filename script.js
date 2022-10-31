b695c150685211d3da9a2ec488d5c69704d5c04f
MD Faizan Alam <97579806+mdfaizanalam2000@users.noreply.github.com>
2022-10-31 23:23:06 +0530

    Add files via upload
    initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const result = document.querySelector("#result");
+const reset = document.querySelector("#reset");
+
+const convert = () => {
+    const inputTemp = document.querySelector("#input").value;
+    const inputTempType = document.querySelector("#tempUnit").value;
+
+    //check if input is empty
+    if (inputTemp == "") {
+        alert("Input cannot be empty");
+        return;
+    }
+
+    let convertedTemp;
+    if (inputTempType == "fahrenheit") {
+        convertedTemp = (inputTemp - 32) * 5 / 9;
+        result.innerText = "Converted temperature is " + convertedTemp.toFixed(2) + "°C";
+    }
+
+    if (inputTempType == "celsius") {
+        convertedTemp = (inputTemp * 9 / 5) + 32;
+        result.innerText = "Converted temperature is " + convertedTemp.toFixed(2) + "°F";
+    }
+}
+
+reset.addEventListener("click", () => {
+    result.innerText = "";
+})
